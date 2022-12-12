50f2d7872cfe08a4baf408a4a4ecb530fba2c350
jvasquezd <jvasquezenrique@gmail.com>
2022-12-12 01:12:16 -0500

    s6 lists y nav

--- semana-6/2-lp-mikhuy/js/index.js
+++ semana-6/2-lp-mikhuy/js/index.js
@@ -2,15 +2,24 @@
 //objeto
 //documento
 
-const iconMenu  = document.getElementById('icon-menu');
-const listHeader  = document.getElementById('list-header');
+const nav = document.querySelector('.nav');
+const iconMenu = document.getElementById('icon-menu');
+const iconClose = document.getElementById('icon-close');
+const listHeader = document.querySelector('.list--header');
+
 
 //const navMenuIcon = document.querySelector('#icon-menu');
 
 iconMenu.addEventListener('click', () =>{
+    listHeader.style.pointerEvents = 'auto';
     listHeader.style.opacity = 1;
 });
 
+
+iconClose.addEventListener('click', () =>{
+    listHeader.style.pointerEvents = 'none';
+    listHeader.style.opacity = 0;
+})
 
 
 //console.log(iconMenu);
