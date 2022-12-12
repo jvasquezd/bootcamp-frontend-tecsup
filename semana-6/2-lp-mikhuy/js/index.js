3f4c697d8384dac5602a395d1fef8e76632d32f7
jvasquezd <jvasquezenrique@gmail.com>
2022-12-12 00:56:46 -0500

    s6 js styles

--- semana-6/2-lp-mikhuy/js/index.js
+++ semana-6/2-lp-mikhuy/js/index.js
@@ -1,22 +1,16 @@
 'use strict';
+//objeto
+//documento
 
-const nav = document.querySelector('.nav');
-const iconMenu = document.getElementById('icon-menu');
-const iconClose = document.getElementById('icon-close');
-const listHeader = document.querySelector('.list--header');
+const iconMenu  = document.getElementById('icon-menu');
+const listHeader  = document.getElementById('list-header');
 
-document.addEventListener('scroll', () => {
-  nav.classList.toggle('nav--scroll', window.scrollY > 0);
+//const navMenuIcon = document.querySelector('#icon-menu');
+
+iconMenu.addEventListener('click', () =>{
+    listHeader.style.opacity = 1;
 });
 
-iconMenu.addEventListener('click', () => {
-  // listHeader.style.pointerEvents = 'auto';
-  // listHeader.style.opacity = 1;
-  listHeader.classList.add('list--header-show');
-});
 
-iconClose.addEventListener('click', () => {
-  // listHeader.style.pointerEvents = 'none';
-  // listHeader.style.opacity = 0;
-  listHeader.classList.remove('list--header-show');
-});
+
+//console.log(iconMenu);
