da0441239caf3bc780e9bd251660133ed59d2134
jvasquezd <jvasquezenrique@gmail.com>
2022-12-14 22:26:34 -0500

    s7 crud pokemons

--- semana-7/1-crud-pokemones/js/index.js
+++ semana-7/1-crud-pokemones/js/index.js
@@ -1,7 +1,54 @@
-'use strict';
+"use strict";
 
 const documentReady = () => {
-  alert('Jhonatan Vasquez')
+  const tBodyPokemons = document.getElementById("tBodyPokemons");
+  const formPokemon = document.getElementById("formPokemon");
+
+  tBodyPokemons.innerHTML += `
+  <tr>
+    <td>1</td>
+    <td>Pikachu</td>
+    <td>Eléctrico</td>
+    <td>250</td>
+    <td>Trueno</td>
+    <td>Voltio</td>
+    <td>
+      <img
+        src="https://images.wikidexcdn.net/mwuploads/wikidex/thumb/7/77/latest/20150621181250/Pikachu.png/1200px-Pikachu.png"
+        alt="Pikachu" class="img-fluid" style="max-width: 128px;" />
+    </td>
+  </tr>
+  `;
+
+  const createPokemon = (e) => {
+    e.preventDefault();
+    const documentFormPokemon = document.forms["formPokemon"];
+
+    const name = documentFormPokemon.name.value;
+    const type = documentFormPokemon.type.value;
+    const hp = documentFormPokemon.hp.value;
+    const attack = documentFormPokemon.attack.value;
+    const special = documentFormPokemon.special.value;
+    const imgUrl = documentFormPokemon.imgUrl.value;
+
+    tBodyPokemons.innerHTML += `
+    <tr>
+      <td>1</td>
+      <td>${name}</td>
+      <td>${type}</td>
+      <td>${hp}</td>
+      <td>${attack}</td>
+      <td>${special}</td>
+      <td>
+        <img
+          src="${imgUrl}"
+          alt="${name}" />
+      </td>
+    </tr>
+  `;
+  };
+
+  formPokemon.addEventListener("submit", createPokemon);
 };
 
-document.addEventListener('DOMContentLoaded', documentReady);
+document.addEventListener("DOMContentLoaded", documentReady);
