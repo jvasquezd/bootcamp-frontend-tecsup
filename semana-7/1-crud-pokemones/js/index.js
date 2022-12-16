7308d8880e65e5ac55bc9ed903b834a368b9845f
jvasquezd <jvasquezenrique@gmail.com>
2022-12-15 23:04:02 -0500

    s7 update index.js delete, read y list

--- semana-7/1-crud-pokemones/js/index.js
+++ semana-7/1-crud-pokemones/js/index.js
@@ -1,47 +1,30 @@
-"use strict";
+'use strict';
 
-class Pokemon {
-  constructor(name, type, hp, attack, special, imgUrl){
-    this.name = name;
-    this.type = type;
-    this.hp = hp;
-    this.attack = attack;
-    this.special = special;
-    this.imgUrl = imgUrl;
-  }
+const POKEMONS_CRUD_DATA = 'pokemons-crud';
+const pokemons = JSON.parse(localStorage.getItem(POKEMONS_CRUD_DATA)) ?? [];
+
+const createPokemon = (e) => {
+  e.preventDefault();
+  const documentFormPokemon = document.forms['formPokemon'];
+  const name = documentFormPokemon.name.value;
+  const type = documentFormPokemon.type.value;
+  const hp = documentFormPokemon.hp.value;
+  const attack = documentFormPokemon.attack.value;
+  const special = documentFormPokemon.special.value;
+  const imgUrl = documentFormPokemon.imgUrl.value;
+  pokemons.push({ name, type, hp, attack, special, imgUrl });
+  localStorage.setItem(POKEMONS_CRUD_DATA, JSON.stringify(pokemons));
+  readPokemons();
 };
 
-
-
-const documentReady = () => {
-  const pokemons = [];
-  //const tBodyPokemons = document.getElementById("tBodyPokemons");
-  const formPokemon = document.getElementById("formPokemon");
-
-  const createPokemon = (e) => {
-    e.preventDefault();
-    const documentFormPokemon = document.forms["formPokemon"];
-
-    const name = documentFormPokemon.name.value;
-    const type = documentFormPokemon.type.value;
-    const hp = documentFormPokemon.hp.value;
-    const attack = documentFormPokemon.attack.value;
-    const special = documentFormPokemon.special.value;
-    const imgUrl = documentFormPokemon.imgUrl.value
-
-    let pokemon = new Pokemon(name,type,hp,attack, special,imgUrl);
-    pokemons.push(pokemon);
-    readPokemons();
-  };
-
-  const readPokemons = () => {
-    const tBodyPokemons = document.getElementById("tBodyPokemons");
-    tBodyPokemons.innerHTML = '';
-    pokemons.forEach((element, index) =>{
-      const {name, type, hp, attack, special, imgUrl} = element;
-      tBodyPokemons.innerHTML += `
+const readPokemons = () => {
+  const tBodyPokemons = document.getElementById('tBodyPokemons');
+  tBodyPokemons.innerHTML = '';
+  pokemons.forEach((element, index) => {
+    const { name, type, hp, attack, special, imgUrl } = element;
+    tBodyPokemons.innerHTML += `
       <tr>
-        <td>${index}</td>
+        <td>${index + 1}</td>
         <td>${name}</td>
         <td>${type}</td>
         <td>${hp}</td>
@@ -50,14 +33,78 @@ const documentReady = () => {
         <td>
           <img
             src="${imgUrl}"
-            alt="${name}" class="img-fluid" style="max-width: 128px;"/>
+            alt="${name}" class="img-fluid" style="max-width: 128px;" />
         </td>
-      </tr>`
-    });
-    
-  };
-
-  formPokemon.addEventListener("submit", createPokemon);
+        <td>
+          <button 
+            class="btn btn-info m-1"
+            onclick="readPokemon(${index})"
+          >
+            ✏
+          </button>
+          <button 
+            class="btn btn-danger m-1"
+            onclick="deletePokemon(${index})"
+          >
+            🗑
+          </button>
+        </td>
+      </tr>
+    `;
+  });
 };
 
-document.addEventListener("DOMContentLoaded", documentReady);
+const readPokemon = (index) => {
+  // const pokemon = pokemons.find((_, i) => {
+  //   return i === index;
+  // });
+  const pokemon = pokemons.slice(index, index + 1)[0];
+  console.log(pokemon);
+};
+
+const deletePokemon = (index) => {
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-success mx-1',
+      cancelButton: 'btn btn-danger mx-1'
+    },
+    buttonsStyling: false
+  });
+  swalWithBootstrapButtons.fire({
+    title: '¿Está seguro?',
+    text: "¡No podrás revertir esto!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: '¡Sí, elimínalo!',
+    cancelButtonText: '¡No, cancélalo!',
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {
+      pokemons.splice(index, 1);
+      localStorage.setItem(POKEMONS_CRUD_DATA, JSON.stringify(pokemons));
+      readPokemons();
+      swalWithBootstrapButtons.fire(
+        '¡Eliminado!',
+        'Tu registro ha sido eliminado.',
+        'success'
+      );
+    } else if (
+      result.dismiss === Swal.DismissReason.cancel
+    ) {
+      swalWithBootstrapButtons.fire(
+        'Cancelado',
+        'Tu registro está seguro',
+        'error'
+      );
+    }
+  });
+};
+
+const documentReady = () => {
+  const formPokemon = document.getElementById('formPokemon');
+
+  readPokemons();
+  formPokemon.addEventListener('submit', createPokemon);
+};
+
+document.addEventListener('DOMContentLoaded', documentReady);
